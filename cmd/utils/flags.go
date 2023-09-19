8ebfd7c196bcd34a37e91a76a7af45eb7365b163
Ferran Borreguero <ferranbt@protonmail.com>
2023-09-19 09:29:55 +0200

    Use a 'name' field to namespace the data directory

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1500,6 +1500,13 @@ func SetDataDir(ctx *cli.Context, cfg *node.Config) {
 		cfg.DataDir = filepath.Join(node.DefaultDataDir(), "sepolia")
 	case ctx.Bool(SuaveFlag.Name) && cfg.DataDir == node.DefaultDataDir():
 		cfg.DataDir = filepath.Join(node.DefaultDataDir(), "suave")
+	case ctx.IsSet(CustomChainFlag.Name) && cfg.DataDir == node.DefaultDataDir():
+		genesis, err := readJSONGenesis(ctx.String(CustomChainFlag.Name))
+		if err == nil {
+			// best effort, it will raise an error later on, we do not need to
+			// catch it here.
+			cfg.DataDir = filepath.Join(node.DefaultDataDir(), genesis.Name)
+		}
 	}
 }
 
@@ -2263,6 +2270,8 @@ func MakeConsolePreloads(ctx *cli.Context) []string {
 type jsonGenesis struct {
 	core *core.Genesis `json:"-"`
 
+	Name string `json:"name"`
+
 	Config struct {
 		Clique struct {
 			// Signers is the list of initial validators of the clique protocol
@@ -2285,6 +2294,9 @@ func readJSONGenesis(genesisPath string) (*jsonGenesis, error) {
 	genesis := new(jsonGenesis)
 	if err := json.Unmarshal(genesisRaw, genesis); err != nil {
 		return nil, fmt.Errorf("invalid genesis file: %v", err)
+	}
+	if genesis.Name == "" {
+		return nil, fmt.Errorf("genesis.json file must have a name field")
 	}
 
 	genesis.core = coreGenesis
