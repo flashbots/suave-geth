396d475c574d7c258dc6f65876a02410c6127ff5
Ferran Borreguero <ferranbt@protonmail.com>
2023-10-18 12:00:51 +0200

    Revert

--- core/vm/suave_abis.go
+++ core/vm/suave_abis.go
@@ -1,3 +1,10 @@
 package vm
 
+import "github.com/ethereum/go-ethereum/accounts/abi"
+
 var bidIdsAbi = mustParseMethodAbi(`[{"inputs": [{ "type": "bytes16[]" }], "name": "bidids", "outputs":[], "type": "function"}]`, "bidids")
+
+func mustParseMethodAbi(data string, method string) abi.Method {
+	inoutAbi := mustParseAbi(data)
+	return inoutAbi.Methods[method]
+}
