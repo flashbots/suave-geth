86987cb4556e21032c499429fec8f040dd59a6cb
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2023-10-24 16:18:06 +0200

    Removes debug return

--- core/vm/contracts_suave_runtime_adapter.go
+++ core/vm/contracts_suave_runtime_adapter.go
@@ -422,7 +422,6 @@ func (b *SuaveRuntimeAdapter) submitBundleToBuilder(input []byte) (res []byte, e
 	)
 
 	if output1, err = b.impl.submitBundleToBuilder(builderUrl, bid); err != nil {
-		res = []byte(err.Error())
 		return
 	}
 
