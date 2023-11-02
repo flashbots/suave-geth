7171c505ae57e396e81849cf60ce1059200f6ad9
Ferran Borreguero <ferranbt@protonmail.com>
2023-11-02 11:57:48 +0100

    Add example

--- suave/sol/standard_peekers/example.sol
+++ suave/sol/standard_peekers/example.sol
@@ -4,8 +4,11 @@ import "../libraries/Suave.sol";
 
 contract ExampleEthCallSource {
     function callTarget(address target, uint256 expected) public {
-        bytes memory output = Suave.ethcall(target, abi.encodeWithSignature("get()"));
-        (uint256 num) = abi.decode(output, (uint64));
+        bytes memory output = Suave.ethcall(
+            target,
+            abi.encodeWithSignature("get()")
+        );
+        uint256 num = abi.decode(output, (uint64));
         require(num == expected);
     }
 }
@@ -13,5 +16,34 @@ contract ExampleEthCallSource {
 contract ExampleEthCallTarget {
     function get() public view returns (uint256) {
         return 101;
+    }
+}
+
+contract OnlyConfidential {
+    event SimResultEvent(uint64 egp);
+
+    function fetchBidConfidentialBundleData() public returns (bytes memory) {
+        require(Suave.isConfidential());
+
+        bytes memory confidentialInputs = Suave.confidentialInputs();
+        return abi.decode(confidentialInputs, (bytes));
+    }
+
+    // note: because of confidential execution,
+    // you will not see your input as input to the function
+    function helloWorld() external payable {
+        // 0. ensure confidential execution
+        require(Suave.isConfidential());
+
+        // 1. fetch bundle data
+        bytes memory bundleData = this.fetchBidConfidentialBundleData();
+
+        // 2. sim bundle and get effective gas price
+        uint64 effectiveGasPrice = Suave.simulateBundle(bundleData);
+
+        emit SimResultEvent(effectiveGasPrice);
+
+        // note: this function doesn't return anything
+        // so this computation result will never land onchain
     }
 }
