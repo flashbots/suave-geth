0511960e0106fc636e14e9c45ce2af99c39ccdf3
Ferran Borreguero <ferranbt@protonmail.com>
2024-02-05 09:09:56 +0000

    Use a different lib for abi parsing + add more methods (#184)

--- suave/consolelog/consolelog_test.go
+++ suave/consolelog/consolelog_test.go
@@ -4,28 +4,42 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/require"
+	"github.com/umbracle/ethgo/abi"
 )
 
 func TestConsoleLog(t *testing.T) {
-	data := emitConsoleLog("log(address)", common.Address{})
-	val, err := decode(data)
-	require.NoError(t, err)
-	require.NotNil(t, val)
+	cases := []struct {
+		typStr string
+		args   interface{}
+	}{
+		{
+			"log(address)", []interface{}{common.Address{}},
+		},
+		{
+			"log(bytes)", []interface{}{[]byte{0x1, 0x2, 0x3}},
+		},
+	}
+
+	for _, c := range cases {
+		data := emitConsoleLog(c.typStr, c.args)
+		val, err := decode(data)
+		require.NoError(t, err)
+		require.NotNil(t, val)
+	}
 }
 
 func emitConsoleLog(typStr string, args interface{}) []byte {
 	// decode the type and encode the arguments
-	typ, err := abi.NewTypeFromString(typStr[strings.Index(typStr, "("):])
+	typ, err := abi.NewType(typStr[strings.Index(typStr, "("):])
 	if err != nil {
 		panic(err)
 	}
 
 	// pack the arguments
-	data, err := typ.Pack(args)
+	data, err := typ.Encode(args)
 	if err != nil {
 		panic(err)
 	}
