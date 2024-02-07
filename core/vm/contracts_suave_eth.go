bbe8ada2f4f52855f79233157a5376621f1d388c
Louis Thibault <l.thibault@sentimens.com>
2024-02-07 11:51:11 -0500

    Build Deneb block in buildEthBlock.

--- core/vm/contracts_suave_eth.go
+++ core/vm/contracts_suave_eth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"math/big"
 	"net/http"
@@ -20,12 +21,12 @@ import (
 	"github.com/flashbots/go-boost-utils/ssz"
 	"github.com/holiman/uint256"
 
-	builderCapella "github.com/attestantio/go-builder-client/api/capella"
+	builderDeneb "github.com/attestantio/go-builder-client/api/deneb"
 	builderV1 "github.com/attestantio/go-builder-client/api/v1"
 	"github.com/attestantio/go-eth2-client/spec/bellatrix"
 	specCapella "github.com/attestantio/go-eth2-client/spec/capella"
+	specDeneb "github.com/attestantio/go-eth2-client/spec/deneb"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
-	boostTypes "github.com/flashbots/go-boost-utils/types"
 	boostUtils "github.com/flashbots/go-boost-utils/utils"
 )
 
@@ -217,7 +218,7 @@ func (b *suaveRuntime) buildEthBlock(blockArgs types.BuildBlockArgs, dataID type
 
 	log.Info("built block from bundles", "payload", *envelope.ExecutionPayload)
 
-	payload, err := executableDataToCapellaExecutionPayload(envelope.ExecutionPayload)
+	payload, err := executableDataToDenebExecutionPayload(envelope.ExecutionPayload)
 	if err != nil {
 		return nil, nil, fmt.Errorf("could not format execution payload as capella payload: %w", err)
 	}
@@ -259,7 +260,7 @@ func (b *suaveRuntime) buildEthBlock(blockArgs types.BuildBlockArgs, dataID type
 		return nil, nil, fmt.Errorf("could not sign builder record: %w", err)
 	}
 
-	bidRequest := builderCapella.SubmitBlockRequest{
+	bidRequest := builderDeneb.SubmitBlockRequest{
 		Message:          &blockBidMsg,
 		ExecutionPayload: payload,
 		Signature:        signature,
@@ -307,7 +308,7 @@ func (b *suaveRuntime) submitEthBlockToRelay(relayUrl string, builderDataRecordJ
 	return resp, nil
 }
 
-func executableDataToCapellaExecutionPayload(data *engine.ExecutableData) (*specCapella.ExecutionPayload, error) {
+func executableDataToDenebExecutionPayload(data *engine.ExecutableData) (*specDeneb.ExecutionPayload, error) {
 	transactionData := make([]bellatrix.Transaction, len(data.Transactions))
 	for i, tx := range data.Transactions {
 		transactionData[i] = bellatrix.Transaction(tx)
@@ -323,13 +324,12 @@ func executableDataToCapellaExecutionPayload(data *engine.ExecutableData) (*spec
 		}
 	}
 
-	baseFeePerGas := new(boostTypes.U256Str)
-	err := baseFeePerGas.FromBig(data.BaseFeePerGas)
-	if err != nil {
-		return nil, err
+	baseFeePerGas := new(uint256.Int)
+	if baseFeePerGas.SetFromBig(data.BaseFeePerGas) {
+		return nil, errors.New("base fee per gas: overflow")
 	}
 
-	return &specCapella.ExecutionPayload{
+	return &specDeneb.ExecutionPayload{
 		ParentHash:    [32]byte(data.ParentHash),
 		FeeRecipient:  [20]byte(data.FeeRecipient),
 		StateRoot:     [32]byte(data.StateRoot),
@@ -341,7 +341,7 @@ func executableDataToCapellaExecutionPayload(data *engine.ExecutableData) (*spec
 		GasUsed:       data.GasUsed,
 		Timestamp:     data.Timestamp,
 		ExtraData:     data.ExtraData,
-		BaseFeePerGas: *baseFeePerGas,
+		BaseFeePerGas: baseFeePerGas,
 		BlockHash:     [32]byte(data.BlockHash),
 		Transactions:  transactionData,
 		Withdrawals:   withdrawalData,
