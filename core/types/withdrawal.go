90c099ec55f1505dd357d37fe01e0b1d1040db17
Jinsuk Park <totorovirus@gmail.com>
2024-02-23 23:29:41 -0800

    Json Marshal / Unmarshal for suave_structs

--- core/types/withdrawal.go
+++ core/types/withdrawal.go
@@ -19,12 +19,21 @@ package types
 import (
 	"bytes"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
 //go:generate go run github.com/fjl/gencodec -type Withdrawal -field-override withdrawalMarshaling -out gen_withdrawal_json.go
 //go:generate go run ../../rlp/rlpgen -type Withdrawal -out gen_withdrawal_rlp.go
+
+// Withdrawal represents a validator withdrawal from the consensus layer.
+type Withdrawal struct {
+	Index     uint64         `json:"index"`          // monotonically increasing identifier issued by consensus layer
+	Validator uint64         `json:"validatorIndex"` // index of validator associated with withdrawal
+	Address   common.Address `json:"address"`        // target address for withdrawn ether
+	Amount    uint64         `json:"amount"`         // value of withdrawal in Gwei
+}
 
 // field type overrides for gencodec
 type withdrawalMarshaling struct {
