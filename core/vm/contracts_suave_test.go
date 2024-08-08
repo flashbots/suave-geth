67e518bdbc35fa429defe6c0d5fb249fc7f1a78f
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-08-08 14:07:20 -0700

    set better example in test

--- core/vm/contracts_suave_test.go
+++ core/vm/contracts_suave_test.go
@@ -2,6 +2,7 @@ package vm
 
 import (
 	"context"
+	"crypto/rand"
 	"math/big"
 	"net/http"
 	"net/http/httptest"
@@ -16,6 +17,7 @@ import (
 	suave "github.com/ethereum/go-ethereum/suave/core"
 	"github.com/ethereum/go-ethereum/suave/cstore"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/scrypt"
 )
 
 type mockSuaveBackend struct {
@@ -141,12 +143,19 @@ func TestSuave_AESPrecompiles(t *testing.T) {
 	b := newTestBackend(t)
 
 	message := []byte("hello world")
-	// any 32-byte value will do; if your secret is not 32 bytes, it's right-padded with 0s
-	secret := crypto.Keccak256([]byte("secret"))
-	ciphertext, err := b.aesEncrypt(secret, message)
+
+	// safely generate a 32-byte secret
+	salt := make([]byte, 32)
+	_, err := rand.Read(salt)
+	require.NoError(t, err)
+	pk, err := scrypt.Key([]byte("some password"), salt, 32768, 8, 1, 32)
+
+	// note: any 32-byte value will do for pk
+	// if your secret is not 32 bytes long, it's right-padded with 0s
+	ciphertext, err := b.aesEncrypt(pk, message)
 	require.NoError(t, err)
 
-	decrypted, err := b.aesDecrypt(secret, ciphertext)
+	decrypted, err := b.aesDecrypt(pk, ciphertext)
 	require.NoError(t, err)
 	require.Equal(t, message, decrypted)
 }
